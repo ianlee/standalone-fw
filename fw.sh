aeed76a964990df3b3a098687f63ad4ccb4f8033
ian <ian.sean.lee@gmail.com>
2014-01-30 16:54:48 -0800

    added interface

--- fw.sh
+++ fw.sh
@@ -39,49 +39,49 @@ iptables -P INPUT DROP
 
 #block inbound traffic from specific IPs 
 iptables -N blockin
-iptables -A blockin -s $IP_BLOCK -j DROP
+iptables -A blockin -i $INTERFACE -s $IP_BLOCK -j DROP
 iptables -A INPUT -j blockin
 
 #block outbound traffic from specific IPs
 iptables -N blockout
-iptables -A blockout -d $IP_BLOCK -j DROP
+iptables -A blockout -o $INTERFACE -d $IP_BLOCK -j DROP
 iptables -A OUTPUT -j blockout
 
 
 
 #create udpin chain
 iptables -N udpin
-iptables -A udpin -p udp --sport $BLOCK_PORTS_IN -j DROP
-iptables -A udpin -p udp --dport $BLOCK_PORTS_IN -j DROP
-iptables -A udpin -p udp -m multiport --sports $DNS_PORT_IN,$DHCP_PORT_IN -j ACCEPT
-iptables -A udpin -p udp -m multiport --sports $UDP_ALLOW_PORTS_IN -m state --state ESTABLISHED -j ACCEPT # acting as a client
-iptables -A udpin -p udp -m multiport --dports $UDP_ALLOW_PORTS_IN_SERVER -m state --state NEW,ESTABLISHED -j ACCEPT # acting as a server
+iptables -A udpin -i $INTERFACE -p udp --sport $BLOCK_PORTS_IN -j DROP
+iptables -A udpin -i $INTERFACE -p udp --dport $BLOCK_PORTS_IN -j DROP
+iptables -A udpin -i $INTERFACE -p udp -m multiport --sports $DNS_PORT_IN,$DHCP_PORT_IN -j ACCEPT
+iptables -A udpin -i $INTERFACE -p udp -m multiport --sports $UDP_ALLOW_PORTS_IN -m state --state ESTABLISHED -j ACCEPT # acting as a client
+iptables -A udpin -i $INTERFACE -p udp -m multiport --dports $UDP_ALLOW_PORTS_IN_SERVER -m state --state NEW,ESTABLISHED -j ACCEPT # acting as a server
 iptables -A INPUT -p udp -j udpin
 #create tcpin chain
 iptables -N tcpin
-iptables -A tcpin -p tcp --sport $BLOCK_PORTS_IN -j DROP
-iptables -A tcpin -p tcp --dport $BLOCK_PORTS_IN -j DROP
-iptables -A tcpin -p tcp --dport 80 -m multiport --sports 0:1023  -j DROP		#drop packets to port 80 from ports less than 1024
-iptables -A tcpin -p tcp -m multiport --sports $DNS_PORT_IN,$DHCP_PORT_IN -j ACCEPT
-iptables -A tcpin -p tcp -m multiport --sports $TCP_ALLOW_PORTS_IN -m state --state ESTABLISHED -j ACCEPT # acting as a client
-iptables -A tcpin -p tcp -m multiport --dports $TCP_ALLOW_PORTS_IN_SERVER -m state --state NEW,ESTABLISHED -j ACCEPT # acting as a server
+iptables -A tcpin -i $INTERFACE -p tcp --sport $BLOCK_PORTS_IN -j DROP
+iptables -A tcpin -i $INTERFACE -p tcp --dport $BLOCK_PORTS_IN -j DROP
+iptables -A tcpin -i $INTERFACE -p tcp --dport 80 -m multiport --sports 0:1023  -j DROP		#drop packets to port 80 from ports less than 1024
+iptables -A tcpin -i $INTERFACE -p tcp -m multiport --sports $DNS_PORT_IN,$DHCP_PORT_IN -j ACCEPT
+iptables -A tcpin -i $INTERFACE -p tcp -m multiport --sports $TCP_ALLOW_PORTS_IN -m state --state ESTABLISHED -j ACCEPT # acting as a client
+iptables -A tcpin -i $INTERFACE -p tcp -m multiport --dports $TCP_ALLOW_PORTS_IN_SERVER -m state --state NEW,ESTABLISHED -j ACCEPT # acting as a server
 iptables -A INPUT -p tcp -j tcpin
 
 #create udpout chain
 iptables -N udpout
-iptables -A udpout -p udp --sport $BLOCK_PORTS_IN -j DROP
-iptables -A udpout -p udp --dport $BLOCK_PORTS_IN -j DROP
-iptables -A udpout -p udp -m multiport --dports $DNS_PORT_OUT,$DHCP_PORT_OUT -j ACCEPT
-iptables -A udpout -p udp -m multiport --dports $UDP_ALLOW_PORTS_OUT -m state --state NEW,ESTABLISHED -j ACCEPT # acting as a client
-iptables -A udpout -p udp -m multiport --sports $UDP_ALLOW_PORTS_OUT_SERVER -m state --state ESTABLISHED -j ACCEPT # acting as a server
+iptables -A udpout -o $INTERFACE -p udp --sport $BLOCK_PORTS_IN -j DROP
+iptables -A udpout -o $INTERFACE -p udp --dport $BLOCK_PORTS_IN -j DROP
+iptables -A udpout -o $INTERFACE -p udp -m multiport --dports $DNS_PORT_OUT,$DHCP_PORT_OUT -j ACCEPT
+iptables -A udpout -o $INTERFACE -p udp -m multiport --dports $UDP_ALLOW_PORTS_OUT -m state --state NEW,ESTABLISHED -j ACCEPT # acting as a client
+iptables -A udpout -o $INTERFACE -p udp -m multiport --sports $UDP_ALLOW_PORTS_OUT_SERVER -m state --state ESTABLISHED -j ACCEPT # acting as a server
 iptables -A OUTPUT -p udp -j udpout
 #create tcpout chain
 iptables -N tcpout
-iptables -A tcpout -p tcp --sport $BLOCK_PORTS_IN -j DROP
-iptables -A tcpout -p tcp --dport $BLOCK_PORTS_IN -j DROP
-iptables -A tcpout -p tcp -m multiport --dports $DNS_PORT_OUT,$DHCP_PORT_OUT -j ACCEPT
-iptables -A tcpout -p tcp -m multiport --dports $TCP_ALLOW_PORTS_OUT -m state --state NEW,ESTABLISHED -j ACCEPT # acting as a client
-iptables -A tcpout -p tcp -m multiport --sports $TCP_ALLOW_PORTS_OUT_SERVER -m state --state ESTABLISHED -j ACCEPT # acting as a server
+iptables -A tcpout -o $INTERFACE -p tcp --sport $BLOCK_PORTS_IN -j DROP
+iptables -A tcpout -o $INTERFACE -p tcp --dport $BLOCK_PORTS_IN -j DROP
+iptables -A tcpout -o $INTERFACE -p tcp -m multiport --dports $DNS_PORT_OUT,$DHCP_PORT_OUT -j ACCEPT
+iptables -A tcpout -o $INTERFACE -p tcp -m multiport --dports $TCP_ALLOW_PORTS_OUT -m state --state NEW,ESTABLISHED -j ACCEPT # acting as a client
+iptables -A tcpout -o $INTERFACE -p tcp -m multiport --sports $TCP_ALLOW_PORTS_OUT_SERVER -m state --state ESTABLISHED -j ACCEPT # acting as a server
 iptables -A OUTPUT -p tcp -j tcpout
 
 
