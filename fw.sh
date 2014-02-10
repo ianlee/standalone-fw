dfa3f9697f64d82d7554ce95a71c64806af9cac1
ian <ian.sean.lee@gmail.com>
2014-02-10 14:12:35 -0800

    test script started

--- fw.sh
+++ fw.sh
@@ -140,6 +140,9 @@ iptables -A blockin -i $EXTERNAL -o $INTERNAL -p tcp -m multiport --dports $BLOC
 #drop SYN packets from ports less than 1024
 iptables -A blockin -i $EXTERNAL -o $INTERNAL -p tcp -m multiport --sports 0:1023 -m state --state NEW -j DROP
 iptables -A blockin -i $EXTERNAL -o $INTERNAL -p udp -m multiport --sports 0:1023 -m state --state NEW -j DROP
+#drop SYN packets to high ports
+iptables -A blockin -i $EXTERNAL -o $INTERNAL -p tcp -m multiport ! --dports 0:1023 -m state --state NEW -j DROP
+iptables -A blockin -i $EXTERNAL -o $INTERNAL -p udp -m multiport ! --dports 0:1023 -m state --state NEW -j DROP
 #Block all external traffic directed to ports 32768 – 32775, 137 – 139, TCP ports 111 and 515. 
 iptables -A blockin -i $EXTERNAL -o $INTERNAL -p tcp -m multiport --dports 32768:32775,137:139,111,515 -j DROP
 iptables -A blockin -i $EXTERNAL -o $INTERNAL -p udp -m multiport --dports 32768:32775,137:139 -j DROP
@@ -165,6 +168,9 @@ iptables -A blockout -o $EXTERNAL -i $INTERNAL -p tcp -m multiport --dports $BLO
 #drop SYN packets from ports less than 1024
 iptables -A blockout -o $EXTERNAL -i $INTERNAL -p tcp -m multiport --sports 0:1023 -m state --state NEW -j DROP
 iptables -A blockout -o $EXTERNAL -i $INTERNAL -p udp -m multiport --sports 0:1023 -m state --state NEW -j DROP
+#drop SYN packets to high ports
+iptables -A blockout -o $EXTERNAL -i $INTERNAL -p tcp -m multiport ! --dports 0:1023 -m state --state NEW -j DROP
+iptables -A blockout -o $EXTERNAL -i $INTERNAL -p udp -m multiport ! --dports 0:1023 -m state --state NEW -j DROP
 #add outbound blocking chain to output chain
 iptables -A FORWARD -j blockout
 
--- testint.sh
+++ testint.sh
@@ -1,10 +1,21 @@
 #testing firewall from an internal source.
 
 IP="192.168.0.3"
-OUTPUT=""
-#ssh into dest ip
-echo "root\nuest1onQ? \n exit\n" | ssh $IP $OUTPUT
+BASEFILE="tests/test"
+CASE=1
 
+#testcase1
+hping3 $IP -c 5  > $BASEFILE$CASE
+#testcase2
+#testcase3
+#testcase4
+#testcase5
+#testcase6
+#testcase7
+#testcase8
+#testcase9
+#testcase10
+#testcase11
+#testcase12
+#testcase13
 
-#iptables -Z
-iptables -L -n -v -x -Z
